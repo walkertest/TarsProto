c0ba28c29a686480336e62fdcdea9a85fc909074
walker <15108309406@yeah.net>
2020-04-19 18:05:01 +0800

    init

---
+++ src/main/java/com/tencent/protodemo/Test.java
@@ -0,0 +1,11 @@
+package com.tencent.protodemo;
+
+/**
+ * Created by walker on 2020/4/19.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
