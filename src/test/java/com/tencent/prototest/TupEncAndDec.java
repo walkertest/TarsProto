b21462dd3b1b08dff210b88ae3d3dbae35c8809b
walker <15108309406@yeah.net>
2020-04-20 00:03:37 +0800

    add

---
+++ src/test/java/com/tencent/prototest/TupEncAndDec.java
@@ -0,0 +1,15 @@
+package com.tencent.prototest;
+
+import org.junit.Test;
+
+/**
+ * Created by walker on 2020/4/19.
+ */
+public class TupEncAndDec {
+
+    @Test
+    public void test() {
+        System.out.println("hello");
+
+    }
+}
